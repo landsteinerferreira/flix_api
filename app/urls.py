664c750face5cdabc4e1f39f5757b2540948faf2
“landsteiner” <landsteinerferreira@hotmail.com>
2026-03-08 09:04:30 -0300

    Creating version control for the project using URLs

--- app/urls.py
+++ app/urls.py
@@ -1,23 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
-from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestoyView
+from django.urls import path, include
 
 
 urlpatterns = [
+    
     path("admin/", admin.site.urls),
-    path('genres/', GenreCreateListView.as_view(), name='genre_create_list'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
-    path('actors/', ActorCreateListView.as_view(), name='actor_create_list'),
-    path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-datail-view'),
-    path('movies/', MovieCreateListView.as_view(), name='movie_create_list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
-    path('reviews/', ReviewCreateListView.as_view(), name='review_create_list'),
-    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestoyView.as_view(), name='review-detail-view'),
+
+    path('api/v1/', include('genres.urls')),
+    path('api/v1/', include('actors.urls')),
+    path('api/v1/', include('movies.urls')),
+    path('api/v1/', include('reviews.urls')),
+   
 ]
 
 if settings.DEBUG:
