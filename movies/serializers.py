8728ba81c18e6fda8b51c80c43cb61dedc3b7512
“landsteiner” <landsteinerferreira@hotmail.com>
2026-03-09 15:57:26 -0300

    Creating validation layers within the serializer using the modelserializer and calculating the average.

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,35 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
+from reviews.models import Review
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg'] # Faz a media sozinho
+
+        if rate:
+            return round(rate, 1)
+        return None
+
+
+# Validações de campos
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres.')
+        return value
+    
+
+
+
+
